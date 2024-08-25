0b43727cd8790a5631f487963210e97f29c18df7
vignan198 <vignan.chitikaneni2@mail.dcu.ie>
2024-08-25 21:39:33 +0530

    code for DHTT22 using simulation

---
+++ DHT22.cpp
@@ -0,0 +1,31 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Temperature: "));
+  Serial.print(temperature);
+  Serial.println(F(" C"));
+
+  Serial.print(F("Humidity: "));
+  Serial.print(humidity);
+  Serial.println(F(" %"));
+}
